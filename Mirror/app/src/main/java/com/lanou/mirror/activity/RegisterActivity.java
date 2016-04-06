86ea2a6e8e901e98d103f88272b99cebc5d5139d
王延晨 <13889717077@163.com>
2016-04-06 08:32:07 +0800

    完成登录界面

---
+++ Mirror/app/src/main/java/com/lanou/mirror/activity/RegisterActivity.java
@@ -0,0 +1,26 @@
+package com.lanou.mirror.activity;
+
+import com.lanou.mirror.R;
+import com.lanou.mirror.base.BaseActivity;
+
+/**
+ * Created by wyc on 16/4/5.
+ */
+public class RegisterActivity extends BaseActivity {
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected int setContent() {
+        return R.layout.activity_register;
+
+    }
+}
