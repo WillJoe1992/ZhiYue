541ca2924c934b64f8b8a1d421ac86e4ab4b8ad2
王延晨 <13889717077@163.com>
2016-04-12 14:56:38 +0800

    分享， 是个什么东西

--- Mirror/app/src/main/java/com/lanou/mirror/adapter/SpecialPictureAdapter.java
+++ Mirror/app/src/main/java/com/lanou/mirror/adapter/SpecialPictureAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -14,6 +15,9 @@ import com.lanou.mirror.R;
 import com.lanou.mirror.base.BaseApplication;
 import com.lanou.mirror.bean.JSONSpecial;
 import com.lanou.mirror.tool.GetScreenHeight;
+
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 
 /**
  * Created by SAZ 16/3/29.
@@ -43,6 +47,39 @@ public class SpecialPictureAdapter extends RecyclerView.Adapter<SpecialPictureAd
             ViewGroup.LayoutParams layoutParams = specialPictureHolder.specialContentPictureItemRv.getLayoutParams();
             layoutParams.height= GetScreenHeight.getScreenHeight(context);
             specialPictureHolder.specialContentPictureItemRv.setLayoutParams(layoutParams);
+            specialPictureHolder.shareBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ShareSDK.initSDK(BaseApplication.getContext());
+                    OnekeyShare oks = new OnekeyShare();
+                    //关闭sso授权
+                    oks.disableSSOWhenAuthorize();
+
+                    // 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+                    //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+                    // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+                    oks.setTitle("mob分享");
+                    // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+                    oks.setTitleUrl("http://sharesdk.cn");
+                    // text是分享文本，所有平台都需要这个字段
+                    oks.setText("我是分享文本");
+                    // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+                    //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+                    // url仅在微信（包括好友和朋友圈）中使用
+                    oks.setUrl("http://sharesdk.cn");
+                    // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+                    oks.setComment("我是测试评论文本");
+                    // site是分享此内容的网站名称，仅在QQ空间使用
+//                    oks.setSite(getString(R.string.app_name));
+                    // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+                    oks.setSiteUrl("http://sharesdk.cn");
+
+                    // 启动分享GUI
+                    oks.show(BaseApplication.getContext());
+
+                }
+            });
+
 
         } else {
             //加载空的布局
@@ -81,6 +118,7 @@ public class SpecialPictureAdapter extends RecyclerView.Adapter<SpecialPictureAd
         LinearLayout specialContentPictureNullItemView;
         ImageView specialBack;
         TextView specialContentItemShareTvTitle,specialContentItemShareTvBigTitle,specialContentItemShareTvContent;
+        Button shareBtn;
         public SpecialPictureHolder(View itemView) {
             super(itemView);
             specialContentPictureItemRv = (RelativeLayout) itemView.findViewById(R.id.special_content_picture_item_rv);
@@ -88,6 +126,7 @@ public class SpecialPictureAdapter extends RecyclerView.Adapter<SpecialPictureAd
             specialContentItemShareTvTitle= (TextView) itemView.findViewById(R.id.special_content_item_share_tv_title);
             specialContentItemShareTvBigTitle= (TextView) itemView.findViewById(R.id.special_content_item_share_tv_big_title);
             specialContentItemShareTvContent= (TextView) itemView.findViewById(R.id.special_content_item_share_tv_content);
+            shareBtn = (Button) itemView.findViewById(R.id.special_content_item_share_btn);
             specialBack= (ImageView) itemView.findViewById(R.id.special_back);
         }
     }
