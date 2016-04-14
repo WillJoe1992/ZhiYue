f43e446cb086046f54fa565d3e940f958a599b15
王延晨 <13889717077@163.com>
2016-04-14 11:58:50 +0800

    小意思

--- Mirror/app/src/main/java/com/lanou/mirror/activity/AddAddressActivity.java
+++ Mirror/app/src/main/java/com/lanou/mirror/activity/AddAddressActivity.java
@@ -1,5 +1,8 @@
 package com.lanou.mirror.activity;
 
+import android.view.View;
+import android.widget.ImageView;
+
 import com.lanou.mirror.R;
 import com.lanou.mirror.base.BaseActivity;
 
@@ -7,15 +10,27 @@ import com.lanou.mirror.base.BaseActivity;
  * Created by wyc on 16/4/13.
  */
 public class AddAddressActivity extends BaseActivity {
+    private ImageView closeIv;
 
     @Override
     protected void initData() {
+        back();
 
     }
 
     @Override
     protected void initView() {
+        closeIv = bindView(R.id.add_address_close);
 
+    }
+
+    public void back(){
+        closeIv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     @Override
--- Mirror/app/src/main/java/com/lanou/mirror/activity/AllAddressActivity.java
+++ Mirror/app/src/main/java/com/lanou/mirror/activity/AllAddressActivity.java
@@ -2,8 +2,7 @@ package com.lanou.mirror.activity;
 
 import android.content.Intent;
 import android.view.View;
-import android.widget.ImageView;
-
+import android.
 import com.lanou.mirror.R;
 import com.lanou.mirror.base.BaseActivity;
 
@@ -11,20 +10,33 @@ import com.lanou.mirror.base.BaseActivity;
  * Created by wyc on 16/4/13.
  */
 public class AllAddressActivity extends BaseActivity {
-    private ImageView addAdressIv;
+    private ImageView addAdressIv,closeIv;
     @Override
     protected void initData() {
-
+        jump();
+        back();
     }
 
     @Override
     protected void initView() {
         addAdressIv = bindView(R.id.add_address_jump);
+        closeIv = bindView(R.id.all_address_close);
+
+    }
+    public void jump(){
         addAdressIv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(AllAddressActivity.this,AddAddressActivity.class);
                 startActivity(intent);
+            }
+        });
+    }
+    public void back(){
+        closeIv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
